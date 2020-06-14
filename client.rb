033131b8f9ce121731720f6c1bdc898939fa49df
Ganesh Gunasegaran <me@itsgg.com>
2020-06-14 09:29:28 +0530

    Updated README

--- client.rb
+++ client.rb
@@ -12,17 +12,23 @@ OptionParser.new do |opt|
 end
   .parse!
 
-puts options
-
 response =
   RestClient.get "#{API_ENDPOINT}?year=#{options[:year]}&team=#{options[:team]}"
 
 players = JSON.parse(response.body)
-table = Terminal::Table.new do |t|
-  t << ['PlayerID', 'YearId', 'Team name(s)', 'Batting Average']
-  players.each_with_index do |player, index|
-    t << [player['name'], player['year_id'], player['teams'], player['batting_average']]
-    t << :separator unless index == players.size - 1 
+table =
+  Terminal::Table.new do |t|
+    t << ['PlayerID', 'YearId', 'Team name(s)', 'Batting Average']
+    t << :separator
+    players.each_with_index do |player, index|
+      t <<
+        [
+          player['name'],
+          player['year_id'],
+          player['teams'],
+          player['batting_average']
+        ]
+      t << :separator unless index == players.size - 1
+    end
   end
-end
 puts table
